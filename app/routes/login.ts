332754a24f6db7a7e1e626b9ed39502d4fbad377
Tanner Dunn <tdunn@oaitc.net>
2021-03-25 10:49:02 -0400

    Initial commit. Created basic login page UI

---
+++ app/routes/login.ts
@@ -0,0 +1,8 @@
+import { action } from '@ember/object';
+import Route from '@ember/routing/route';
+import { tracked } from '@glimmer/tracking';
+
+export default class Login extends Route.extend({
+  // anything which *must* be merged to prototype here
+}) {
+}
---
+++ blueprints/sc/files/app/components/__typescriptToken__.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+interface <%= entity.classyName.moduleName %>Args {}
+
+export default class <%= entity.classyName.moduleName %> extends Component<<%= entity.classyName.moduleName %>Args> {}
